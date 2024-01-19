38bb371a0760827a242a533b385c82b6177e70af
Tushar Gagal <tushargagal1209@gmail.com>
2024-01-19 18:32:39 +0530

    added more components

---
+++ src/Components/AuthLayout.jsx
@@ -0,0 +1,22 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+export default function Protected({ children, authentication = true }) {
+  const navigate = useNavigate();
+  const [loader, setLoader] = useState(true);
+  const authStatus = useSelector((state) => state.auth.status);
+
+  useEffect(() => {
+    //TODO : make it more easy to understand
+    if (authentication && authentication !== authStatus) {
+      navigate("/login");
+    } else if (!authentication && authStatus !== authentication) {
+      navigate("/");
+    }
+    setLoader(false);
+  }, [authStatus, navigate, authentication]);
+  return loader ? <h1>...Loading</h1> : <>{children}</>;
+}
--- src/Components/index.js
+++ src/Components/index.js
@@ -3,5 +3,24 @@ import Footer from "./Footer/Footer.jsx";
 import Container from "./Container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
-
-export { Header, Footer, Container, Logo, LogoutBtn };
+import Button from "./Button";
+import Login from "./Login";
+import Input from "./Input";
+import Select from "./Select";
+import PostCard from "./PostCard";
+import Signup from "./Signup";
+import RTE from "./RTE";
+export {
+  Header,
+  Footer,
+  Container,
+  Logo,
+  LogoutBtn,
+  Button,
+  Select,
+  Login,
+  Input,
+  PostCard,
+  Signup,
+  RTE,
+};
